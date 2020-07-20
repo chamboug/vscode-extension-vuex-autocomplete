4ef02d4124edb6c813b44883f26a3aa08f5f7f2e
Gaetan Chambouvet <gct2@3ds.com>
2020-07-20 16:02:34 +0200

    Split into multiple functions

--- src/VuexParser.js
+++ src/VuexParser.js
@@ -4,36 +4,37 @@ const RootFileReader = require("./RootFileReader");
 module.exports = class VuexParser {
     async run() {
         const rootFileContent = await new RootFileReader().getRootFileContent();
-        const parsedRootFileContent = cherow.parseModule(rootFileContent);
-
-        const variableDeclarations = parsedRootFileContent.body.filter(x => x.type === "VariableDeclaration");
+        this.parseFile(rootFileContent);
+    }
+    parseFile(fileContent) {
+        const parsedFileContent = cherow.parseModule(fileContent);
+        const storeNode = this.getStoreNode(parsedFileContent);
+        const modules = this.getPropertyFromStoreNode(storeNode, "modules", parsedFileContent);
+        console.log(modules);
+    }
+    getStoreNode(parsedFileContent) {
+        const variableDeclarations = parsedFileContent.body.filter(x => x.type === "VariableDeclaration");
         const storeVariableDeclaration = variableDeclarations.find(x => x.declarations[0].id.name === "store");
         if (storeVariableDeclaration) {
-            // const store = new Vuex.Store({ ... });
-            // export { store };
-            const moduleProperties = storeVariableDeclaration.declarations[0].init.arguments[0].properties.find(x => x.key.name === "modules");
-            if (moduleProperties.value.properties) {
-                const moduleNames = moduleProperties.value.properties.map(x => x.key.name);
-                console.log(moduleNames);
-            } else {
-                const modulesVariableDeclaration = variableDeclarations.find(x => x.declarations[0].id.name === "modules");
-                const moduleNames = modulesVariableDeclaration.declarations[0].init.properties.map(x => x.key.name);
-                console.log(moduleNames);
-            }
-        } else {
-            // export const store = new Vuex.Store({ ... });
-            const exportNamedDeclarations = parsedRootFileContent.body.filter(x => x.type === "ExportNamedDeclaration");
-            const storeExportNamedDeclaration = exportNamedDeclarations.find(x => x.declaration.declarations[0].id.name === "store");
-            const moduleProperties = storeExportNamedDeclaration.declaration.declarations[0].init.arguments[0].properties.find(x => x.key.name === "modules");
-            if (moduleProperties.value.properties) {
-                const moduleNames = moduleProperties.value.properties.map(x => x.key.name);
-                console.log(moduleNames);
-            } else {
-                const modulesVariableDeclaration = variableDeclarations.find(x => x.declarations[0].id.name === "modules");
-                const moduleNames = modulesVariableDeclaration.declarations[0].init.properties.map(x => x.key.name);
-                console.log(moduleNames);
-            }
+            return { declarationType: "VariableDeclaration", storeDeclaration: storeVariableDeclaration };
+        }
+        const exportNamedDeclarations = parsedFileContent.body.filter(x => x.type === "ExportNamedDeclaration");
+        const storeExportNamedDeclaration = exportNamedDeclarations.find(x => x.declaration.declarations[0].id.name === "store");
+        return { declarationType: "ExportNamedDeclaration", storeDeclaration: storeExportNamedDeclaration };
+    }
+    getPropertyFromStoreNode(storeNode, propertyName, parsedFileContent) {
+        let propertyNode;
+        if (storeNode.declarationType === "VariableDeclaration") {
+            propertyNode = storeNode.storeDeclaration.declarations[0].init.arguments[0].properties.find(x => x.key.name === propertyName);
+        } else { // ExportNamedDeclaration
+            propertyNode = storeNode.storeDeclaration.declaration.declarations[0].init.arguments[0].properties.find(x => x.key.name === propertyName);
         }
 
+        if (propertyNode.value.properties) {
+            return propertyNode.value.properties.map(x => x.key.name);
+        }
+        const variableDeclarations = parsedFileContent.body.filter(x => x.type === "VariableDeclaration");
+        const modulesVariableDeclaration = variableDeclarations.find(x => x.declarations[0].id.name === propertyName);
+        return modulesVariableDeclaration.declarations[0].init.properties.map(x => x.key.name);
     }
 };
