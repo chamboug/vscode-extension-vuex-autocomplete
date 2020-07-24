715350472d05912f7bcc140cdb3ff7c1902e12bb
Gaetan Chambouvet <gct2@3ds.com>
2020-07-24 17:57:45 +0200

    Suggest mapState attribute based on namespace

--- src/CodeCompletion.js
+++ src/CodeCompletion.js
@@ -1,4 +1,6 @@
 const vscode = require("vscode");
+const { parse } = require("@babel/parser");
+const esquery = require('esquery');
 
 class CodeCompletion {
     constructor() {
@@ -13,13 +15,29 @@ class CodeCompletion {
             "vue",
             {
                 provideCompletionItems(document, position) {
-                    const linePrefix = document.lineAt(position).text.substr(0, position.character);
-                    const triggerFunctions = ["mapState", "mapGetters", "mapMutations", "mapActions"];
-                    if (triggerFunctions.every(f => !linePrefix.endsWith(`...${f}("`))) {
-                        return undefined;
+                    try {
+                        const linePrefix = document.lineAt(position).text.substr(0, position.character);
+                        const triggerFunctions = ["mapState", "mapGetters", "mapMutations", "mapActions"];
+                        if (triggerFunctions.some(f => linePrefix.endsWith(`...${f}("`))) {
+                            return storeTree.listNamespaces().map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.Text));
+                        } else {
+                            const scriptLineIndex = document.getText().split("\n").findIndex(l => /<script>/.test(l));
+                            const scriptContent = document.getText().split("<script>")[1].split("</script>")[0];
+                            const ast = parse(scriptContent, { sourceType: "module" });
+                            const componentNode = esquery(ast, "ExportDefaultDeclaration")[0];
+                            const computedPropertiesNode = esquery(componentNode, "ObjectProperty[key.name=\"computed\"]")[0];
+                            const mapStateNodes = esquery(computedPropertiesNode, `SpreadElement[loc.start.line<=${position.line - scriptLineIndex + 1}][loc.end.line>=${position.line - scriptLineIndex + 1}][argument.callee.name="mapState"]`);
+                            if (mapStateNodes.length > 0) {
+                                const [namespace, ...attributes] = esquery(mapStateNodes[0], "StringLiteral");
+                                const state = storeTree.getNodeByNamespacePrefix(namespace.value).state;
+                                return state.map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.Text));
+                            }
+                        }
+                    } catch (e) {
+                        console.log(e)
                     }
-    
-                    return storeTree.listNamespaces().map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.Text));
+
+                    return undefined;
                 }
             },
             "."
--- src/StoreTree.js
+++ src/StoreTree.js
@@ -1,6 +1,7 @@
 module.exports = class StoreTree {
     constructor(tree) {
         this.tree = tree;
+        this.flatTree = this.flat();
     }
     flat() {
         return this.recursiveFlattenTree(this.tree, []);
@@ -14,7 +15,10 @@ module.exports = class StoreTree {
         ]
     }
     listNamespaces() {
-        const namespacePrefixes = this.flat().map(x => x.namespacePrefix);
+        const namespacePrefixes = this.flatTree.map(x => x.namespacePrefix);
         return namespacePrefixes.filter(x => !!x).map(x => x.endsWith("/") ? x.slice(0, -1) : x );
+    }
+    getNodeByNamespacePrefix(namespacePrefix) {
+        return this.flatTree.find(x => x.namespacePrefix === `${namespacePrefix}/`);
     }
 };
