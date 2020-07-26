a91eca76c9344acc43f6e177b879362111ad21cf
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2020-07-26 21:40:32 +0200

    Handle mutations and actions

--- src/CodeCompletion.js
+++ src/CodeCompletion.js
@@ -25,14 +25,23 @@ class CodeCompletion {
                             const scriptContent = document.getText().split("<script>")[1].split("</script>")[0];
                             const ast = parse(scriptContent, { sourceType: "module" });
                             const componentNode = esquery(ast, "ExportDefaultDeclaration")[0];
-                            const computedPropertiesNode = esquery(componentNode, "ObjectProperty[key.name=\"computed\"]")[0];
 
-                            const matchingMapFunction = triggerFunctions.map(f => esquery(computedPropertiesNode, `SpreadElement[loc.start.line<=${position.line - scriptLineIndex + 1}][loc.end.line>=${position.line - scriptLineIndex + 1}][argument.callee.name="${f}"]`)).find(x => x.length > 0);
+                            const computedPropertiesNode = esquery(componentNode, "ObjectProperty[key.name=\"computed\"]")[0];
+                            const matchingMapFunction = ["mapState", "mapGetters"].map(f => esquery(computedPropertiesNode, `SpreadElement[loc.start.line<=${position.line - scriptLineIndex + 1}][loc.end.line>=${position.line - scriptLineIndex + 1}][argument.callee.name="${f}"]`)).find(x => x.length > 0);
                             if (matchingMapFunction) {
                                 const matchingMapFunctionName = esquery(matchingMapFunction[0], "Identifier")[0].name;
                                 const [namespace, ...attributes] = esquery(matchingMapFunction[0], "StringLiteral");
                                 const matchingAttributes = storeTree.getNodeByNamespacePrefix(namespace.value)[matchingMapFunctionName.toLowerCase().slice(3)];
                                 return matchingAttributes.map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.Text));
+                            } else {
+                                const methodsPropertiesNode = esquery(componentNode, "ObjectProperty[key.name=\"methods\"]")[0];
+                                const matchingMapFunction = ["mapMutations", "mapActions"].map(f => esquery(methodsPropertiesNode, `SpreadElement[loc.start.line<=${position.line - scriptLineIndex + 1}][loc.end.line>=${position.line - scriptLineIndex + 1}][argument.callee.name="${f}"]`)).find(x => x.length > 0);
+                                if (matchingMapFunction) {
+                                    const matchingMapFunctionName = esquery(matchingMapFunction[0], "Identifier")[0].name;
+                                    const [namespace, ...attributes] = esquery(matchingMapFunction[0], "StringLiteral");
+                                    const matchingAttributes = storeTree.getNodeByNamespacePrefix(namespace.value)[matchingMapFunctionName.toLowerCase().slice(3)];
+                                    return matchingAttributes.map(item => new vscode.CompletionItem(item, vscode.CompletionItemKind.Text));
+                                }
                             }
                         }
                     } catch (e) {
